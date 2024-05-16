c8fc104eb02a396092206716433ff396d96785d6
Numbsku11 <mswift823@gmail.com>
2024-05-16 13:06:53 +0100

    Ad

---
+++ manuelonthebeat.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+struct englishchar{
+    int numspice;
+    int lengthnose;
+};
+
+
+
+int main() {
+    std::cout << "English Simulator";
+    std::cout <<"\n\nPress Any Button To Continue";
+    std::cin.get();
+
+}
